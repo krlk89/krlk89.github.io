49d79901c03f6ebe98c7cbe4c99a38d6d701ff83
krlk89 <kaarel.koitne@gmail.com>
2019-07-09 11:45:11 +0300

    Better code organization Put mapStyles and places into module files

---
+++ modules/mapStyles.js
@@ -0,0 +1,19 @@
+export const mapStyles = [{
+    "elementType": "labels",
+    "stylers": [{
+        "visibility": "off"
+    }]
+},
+{
+    "featureType": "administrative.land_parcel",
+    "stylers": [{
+        "visibility": "off"
+    }]
+},
+{
+    "featureType": "administrative.neighborhood",
+    "stylers": [{
+        "visibility": "off"
+    }]
+}
+];
---
+++ modules/places.js
@@ -0,0 +1,120 @@
+export const places = {
+    "1": [
+        {
+            "name": "Tallinn",
+            "lat": 59.4372,
+            "lng": 24.745,
+            "description": "Tallinn on Eesti pealinn."
+        },
+        {
+            "name": "Tartu",
+            "lat": 58.38,
+            "lng": 26.7225,
+            "description": "Tartu on 'Heade mõtete linn'."
+        },
+        {
+            "name": "Narva",
+            "lat": 59.3792,
+            "lng": 28.2006,
+            "description": "Narva on suuruselt Eesti kolmas linn."
+        },
+        {
+            "name": "Pärnu",
+            "lat": 58.3844,
+            "lng": 24.4989,
+            "description": "Pärnu on Eesti suvepealinn."
+        }
+    ],
+    "2": [
+        {
+            "name": "Kohtla-Järve",
+            "lat": 59.4,
+            "lng": 27.2833,
+            "description": "Kohtla-Järve elanikkond on valdavalt venekeelne."
+        },
+        {
+            "name": "Viljandi",
+            "lat": 58.3633,
+            "lng": 25.5956,
+            "description": "Viljandi asub Sakala kõrgustikul, Viljandi järve kaldal."
+        },
+        {
+            "name": "Maardu",
+            "lat": 59.4781,
+            "lng": 25.0161,
+            "description": "Maardu asub Muuga lahe kaldal."
+        },
+        {
+            "name": "Rakvere",
+            "lat": 59.3506,
+            "lng": 26.3611,
+            "description": "Rakvere on linn alates 12. juunist 1302."
+        },
+        {
+            "name": "Sillamäe",
+            "lat": 59.3931,
+            "lng": 27.7742,
+            "description": "Sillamäe asub Narva lahe kaldal Sõtke jõe suudmes."
+        },
+        {
+            "name": "Võru",
+            "lat": 57.8486,
+            "lng": 26.9928,
+            "description": "Võru tunnuslause on 'Üts ummamuudu liin'."
+        }
+    ],
+    "3": [
+        {
+            "name": "Kuressaare",
+            "lat": 58.2533,
+            "lng": 22.4861,
+            "description": "Kuressaare on Saaremaa valla keskus."
+        },
+        {
+            "name": "Valga",
+            "lat": 57.7769,
+            "lng": 26.0311,
+            "description": "Valga on eesti kõige lõunapoolsem linn."
+        },
+        {
+            "name": "Jõhvi",
+            "lat": 59.3575,
+            "lng": 27.4122,
+            "description": "Jõhvi."
+        },
+        {
+            "name": "Haapsalu",
+            "lat": 58.9470,
+            "lng": 23.5370,
+            "description": "Haapsalu."
+        }
+    ],
+    "4": [
+        {
+            "name": "Keila",
+            "lat": 59.3081,
+            "lng": 24.4263,
+            "description": "Keila."
+        },
+        {
+            "name": "Paide",
+            "lat": 58.8833,
+            "lng": 25.5667,
+            "description": "Paide."
+        }
+    ],
+    "5": [
+        {
+            "name": "Võiste",
+            "lat": 58.2039,
+            "lng": 24.4771,
+            "description": ":)"
+        },
+        {
+            "name": "Varstu",
+            "lat": 57.6430,
+            "lng": 26.66,
+            "description": ";)"
+        }
+    ]
+};
